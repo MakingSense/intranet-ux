b5ec52c97498f8598b5c8ac9506332489add22c8
Marcos <mrigoli@makingsense.com>
2017-12-11 11:05:14 -0300

    Added indicator when weather is not geolocated

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,6 @@
+var weatherRefreshTimer = false;
+var weatherRefreshTimerInterval = 1000 * 60 * 1; // 1 minute
+
 $(() => {
   'use strict';
   let location = 466863; // Default woeid: mar del plata
@@ -8,50 +11,56 @@ $(() => {
     $('.js-geolocation').hide();
   }
 
-  $("#header").on('click', '#weather', function () {
+  $('#weather').click(function () {
+    let $this = $(this);
     navigator.geolocation.getCurrentPosition(function(p) {
+      console.log(p);
       location = p.coords.latitude + ',' + p.coords.longitude;
-      loadWeather(location);
-      $('.js-geolocation').fadeOut();
-    }, function (p) {
+      loadWeather(location).then(() => {
+        $('.js-geolocation').fadeOut();
+        $this.removeClass('weather--disconnected');
+      });
+    }, (e) => {
+      console.log(e);
       // Oh, bugga...
+      loadWeather(location).then(() => {
+        $this.addClass('weather--disconnected');
+        $this.attr('title', '(Click to locate)\n' + $this.attr('title'))
+      }); // default
     });
-  });
-
-  let refreshWeather = setInterval(() => {
-    loadWeather(location);
-  }, 1000 * 60 * 1); // 1 minute
-
-  loadWeather(location); // Initial load
+  }).click();
 });
 
 
 function loadWeather(query) {
   if (!query) return false;
-
-  let options = {
-    unit: 'c',
-    success: function(weather) {
-      let hint = weather.city + "\r\n" + weather.currently + ' - ' + weather.temp + '&deg; ' + weather.units.temp;
-      let html = '';
-      html += '<div id="weather" class="weather" title="' + hint + '">';
-      html +=   '<div class="weather__tile weather__icon"><i class="icon-' + weather.code + '"></i></div>';
-      html +=   '<div class="weather__tile weather__temp">' + weather.temp + '&deg;</div>';
-      html += '</div>';
-      $('#weather').remove();
-      $("#header").prepend(html);
-    },
-    error: function(e) {
-      console.error('Error in weather widget: ' + e);
+  return new Promise((resolve, reject) => {
+    let options = {
+      unit: 'c',
+      success: function(weather) {
+        let hint = weather.city + "\r\n" + weather.currently + ' - ' + weather.temp + '° ' + weather.units.temp;
+        let html = '';
+        html +=   '<div class="weather__tile weather__icon"><i class="icon-' + weather.code + '"></i></div>';
+        html +=   '<div class="weather__tile weather__temp">' + weather.temp + '&deg;</div>';
+        $('#weather').attr('title', hint);
+        $('#weather').html(html);
+        if (weatherRefreshTimer) {
+          clearInterval(weatherRefreshTimer);
+        }
+        weatherRefreshTimer = setInterval(() => {
+          loadWeather(location);
+        }, weatherRefreshTimerInterval);
+        resolve(weather);
+      },
+      error: function(e) {
+        reject(e);
+      }
+    };
+    if (typeof(query) === 'number') {
+      options.woeid = query;
+    } else {
+      options.location = query;
     }
-  };
-
-  if (typeof(query) === 'number') {
-    options.woeid = query;
-  } else {
-    options.location = query;
-  }
-
-  $.simpleWeather(options);
-
+    $.simpleWeather(options);
+  });
 }
